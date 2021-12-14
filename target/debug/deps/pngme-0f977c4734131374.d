3d3893722f6063ec8b100f4f90ea7f30ddb9b69f
Ashish Thapa <ashish.thapa477@gmail.com>
2021-12-14 05:54:09 +0545

    Delete target directory

--- target/debug/deps/pngme-0f977c4734131374.d
+++
@@ -1,8 +0,0 @@
-/home/cdjk/Documents/github/rust/rustlings/pngme/target/debug/deps/pngme-0f977c4734131374: src/main.rs src/chunk_types.rs src/test.rs src/test/chunk_types_test.rs
-
-/home/cdjk/Documents/github/rust/rustlings/pngme/target/debug/deps/pngme-0f977c4734131374.d: src/main.rs src/chunk_types.rs src/test.rs src/test/chunk_types_test.rs
-
-src/main.rs:
-src/chunk_types.rs:
-src/test.rs:
-src/test/chunk_types_test.rs:
